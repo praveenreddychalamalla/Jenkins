36e6b7edd4ed615224f15c1c65aa344b74b53ca8
Praveen Reddy <54468009+praveenreddychalamalla@users.noreply.github.com>
2022-02-14 22:32:38 +0530

    first commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,11 @@
 from datetime import datetime
-print("Integrating Jenkins with GIT : Feb-14-2022 ")
-
+from datetime import date
+today = date.today()
 now = datetime.now()
 
+todays_date = today.strftime("%B %d, %Y")
 current_time = now.strftime("%H:%M:%S")
+
+print("Integrating Jenkins with GIT")
+print("Date : ", todays_date)
 print("Current Time =", current_time)
