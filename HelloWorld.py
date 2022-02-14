d2d9130dd9b4aafd56b338677f54df1553311501
Praveen Reddy <praveenreddychalamalla@gmail.com>
2022-02-14 22:25:28 +0530

    first commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,7 @@
-print("Integrating Jenkins with GIT : Feb-14-2022")
+from datetime import datetime
+print("Integrating Jenkins with GIT : Feb-14-2022 ")
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
