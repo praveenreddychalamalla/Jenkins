eebe7ac4215d6852d80e994282555cda554cf541
Praveen Reddy <54468009+praveenreddychalamalla@users.noreply.github.com>
2022-02-18 14:39:38 +0530

    minor

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,6 +7,6 @@ now = datetime.now()
 todays_date = today.strftime("%B %d, %Y")
 current_time = now.strftime("%H:%M:%S")
 
-print("Integrating Jenkins with GIT, checking  Webhook ok n/w")
+print("Integrating Jenkins with GIT, checking  Webhook ok N/W")
 print("Date : ", todays_date)
 print("Current Time : ", current_time)
